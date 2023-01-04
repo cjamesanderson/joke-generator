a249aa962889ba5d563950ce790f21fb7ffab46a
Cameron Anderson <cameron.anderson.j@gmail.com>
2023-01-03 17:30:19 -0800

    Beautify Thymeleaf template and change implementation of JokeService to initialize ChuckNorrisQuotes in constructor

--- src/main/java/guru/springframework/jokegenerator/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/jokegenerator/services/JokeServiceImpl.java
@@ -6,7 +6,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class JokeServiceImpl implements JokeService {
 
-    ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+            this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
 
     @Override
     public String getJoke() {
